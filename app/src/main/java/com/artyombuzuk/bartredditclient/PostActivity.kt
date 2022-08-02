90f8d642f825e6ff96aea59056563664645a46b8
artyombuzuk16 <artyombuzuk16@gmail.com>
2022-08-02 11:17:39 +0300

    Initial commit

---
+++ app/src/main/java/com/artyombuzuk/bartredditclient/PostActivity.kt
@@ -0,0 +1,11 @@
+package com.artyombuzuk.bartredditclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
